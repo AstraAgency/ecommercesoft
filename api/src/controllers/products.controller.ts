09f23c3ae135b527c5cdc8cfbe42fdfc7d60ec03
Arviixzuh <arviixzuh@gmail.com>
2024-03-08 00:25:12 -0400

    style: 🌈 corrección de indentado

--- api/src/controllers/products.controller.ts
+++ api/src/controllers/products.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Get,
-  Put,
-  Body,
-  Post,
-  Param,
-  Delete,
-  Controller,
-  ParseIntPipe,
-} from '@nestjs/common';
-import { Products } from '@prisma/client';
-import { ProductsService } from '../services/products.service';
+import { Products } from '@prisma/client'
+import { ProductsService } from '../services/products.service'
+import { Get, Put, Body, Post, Param, Delete, Controller, ParseIntPipe } from '@nestjs/common'
 
 @Controller('products')
 export class ProductsController {
@@ -17,26 +8,26 @@ export class ProductsController {
 
   @Get()
   getAllProducts() {
-    return this.productsService.getAllProducts();
+    return this.productsService.getAllProducts()
   }
 
   @Get(':id')
   getProductById(@Param('id') id: string) {
-    return this.productsService.getProductById(Number(id));
+    return this.productsService.getProductById(Number(id))
   }
 
   @Post()
   createProduct(@Body() data: Products) {
-    return this.productsService.createProduct(data);
+    return this.productsService.createProduct(data)
   }
 
   @Delete(':id')
   deleteProduct(@Param('id') id: string) {
-    return this.productsService.deleteProduct(Number(id));
+    return this.productsService.deleteProduct(Number(id))
   }
 
   @Put(':id')
   updateProduct(@Param('id', ParseIntPipe) id: number, @Body() data: Products) {
-    return this.productsService.updateProduct(id, data);
+    return this.productsService.updateProduct(id, data)
   }
 }
