287d56f039f29f497c26cbcac1717abd2fe2f5cb
Arviixzuh <arviixzuh@gmail.com>
2024-03-08 20:36:10 -0400

    feat: ✨ implementación del sistema para manejar usuarios en la plataforma

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth.module'
 import { AppService } from '../services/app.service'
+import { UsersModule } from './users.module'
 import { AppController } from '../controllers/app.controller'
 import { ProductsModule } from './products.module'
 
 @Module({
-  imports: [ProductsModule, AuthModule],
+  imports: [ProductsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
