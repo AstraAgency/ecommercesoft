8112d8bd74da55b030330155dc1a5ef11d11d7ad
Arviixzuh <arviixzuh@gmail.com>
2024-03-08 20:35:20 -0400

    refactor: 🛠️ eliminación de un interface innecesario

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -2,10 +2,6 @@ import { Users } from '@prisma/client'
 import { AuthService } from 'src/services/auth.service'
 import { Controller, Post, Body, Get, Param } from '@nestjs/common'
 
-interface tokenData {
-  token: string
-}
-
 @Controller('auth')
 export class AuthController {
   constructor(private readonly appService: AuthService) {}
